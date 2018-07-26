e4811320979bb80155b66da512880b6b166d1738
Timur <timur.karimow.95@gmail.com>
2018-07-26 16:28:49 +0200

    Add server files and README

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var express_graphql = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+    type Query {
+        message: String
+    }
+`);
+
+
+var root = {
+    message: () => 'Hello World!'
+};
+
+var app = express();
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(4000, () => console.log('Running on 4000'));
